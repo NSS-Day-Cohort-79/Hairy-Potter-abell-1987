4cd419f8fd8c0c7b977bb05918f21249906f3ca6
Ashley <artsyashley.1987@gmail.com>
2025-11-29 17:39:09 -0500

    Moved PotteryWheel to correct dir. Updated main.js. Deleted duplicate scripts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,13 @@
 // Imports go first
-
+import { makePottery } from './PotteryWheel.js';
 
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
+let mug = makePottery("mug", 1, 5)
+let bowl = makePottery("bowl", 7, 3)
+let stein = makePottery("stein", 10, 6)
+let tangine = makePottery("tangine", 15, 8)
+let statue = makePottery("statue", 100, 50)
 
 
 // Fire each piece of pottery in the kiln
@@ -13,4 +17,3 @@ let mug = makePottery()
 
 
 // Invoke the component function that renders the HTML list
-
